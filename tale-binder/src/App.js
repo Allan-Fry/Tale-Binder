3667e33a6e86e5b61128a17341ed8bf8a095de02
Allan-Fry <allan.fry.it@gmail.com>
2021-01-08 16:08:39 +1300

    Added a react router

--- tale-binder/src/App.js
+++ tale-binder/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={home} />
+        <Route exact path="/Edit" component={edit} />
+        <Route exact path="/Manage" component={manage} />
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
